9e972605817090f600dd5e2ebc0323c63187dd50
karen-castillo-laplateforme <karen.castillo@laplateforme.io>
2025-11-18 14:46:52 +0100

    job1 done

---
+++ jour03/job01/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <p></p>
+  <button id="btn1">Set text</button>
+  <button id="btn2">Hide text</button>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="script.js"></script> 
+</body>
+</html>
---
+++ jour03/job01/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+  $("#btn1").click(function(){
+    $("p").text("Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit,ensuite on prie.");
+  });
+
+  $("#btn2").click(function(){
+    $("p").hide();
+  })
+});
